d73491705667542c03af0b8603104d922710bc3a
Canh Minh Do <minhcanh99@gmail.com>
2021-10-13 10:18:51 +0900

    Update test script

--- test-script.sh
+++ test-script.sh
@@ -2,5 +2,5 @@
 time maude << EOF
 in debug.maude .
 in specs/self-stabilization/k-states.maude
-red in K-STATES-CHECK : modelCheck(init15, cstable)
+red in K-STATES-CHECK : modelCheck(init15, cstable) .
 EOF
